e0f9c4f21fe342450c27902cd54ea9e4e4329c48
unknown <amariop2685@gmail.com>
2019-01-08 20:07:43 -0300

    ajustes para footer

--- servicios/administracionoffice365.php
+++ servicios/administracionoffice365.php
@@ -10,8 +10,8 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:600"> 
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="../css/style.css">
-  <link rel="stylesheet" href="../css/style_nosotros.css">
+  <!--<link rel="stylesheet" href="../css/style.css">-->
+  <!--<link rel="stylesheet" href="../css/style_nosotros.css">-->
   <link rel="stylesheet" href="../css/style_adminoffice.css">
   <link rel="stylesheet" href= "../bootstrap-4.1.3/bootstrap-4.1.3/dist/css/bootstrap.min.css">
   
